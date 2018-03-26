26dd8466bed683ae5a0d70aa261b3953c4fde741
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-03-26 12:46:17 +0100

    Initial setup
    New create-react-app set up
    Then copied files from Exercise Files > Resources
    Added prop-types package converted App.js / grid.js to import prop-types
    renamed .jsx files to .js
    set up eslint config and installed extra eslint packages
    Added styleint + config
    Updated js + css formatting to eslint/styleint valid style

---
+++ src/components/listings/Grid.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Single from './Single';
+
+export default class Grid extends Component {
+    renderList() {
+        return this.props.items.map((item) => (
+            <Single key={item.id} item={item}/>
+        ));
+    }
+
+    render() {
+        return(
+            <div>
+                <div className="row">
+                    <ul>
+                        {this.renderList()}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+Grid.propTypes = {
+    // eslint-disable-next-line
+    items: PropTypes.array.isRequired,
+};
