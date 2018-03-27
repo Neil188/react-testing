7de7823bac3f83314bbf082ce19cb6969a7e9c87
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-03-27 19:11:53 +0100

    Add Grid test
    with example tests for Numbers, Strings, Arrays  and Objects

---
+++ src/components/listings/Grid.test.js
@@ -0,0 +1,40 @@
+import data from '../../data/courses.json';
+
+// Numbers
+const numItems = data.length;
+
+test('Number of items = 12', () => {
+    expect(numItems).toBe(12);
+});
+
+test('Number of items to be greater than 12', () => {
+    expect(numItems).toBeGreaterThanOrEqual(12);
+});
+
+// Strings
+const dataTest = data[0].title;
+
+test('There is a JS in this title', () => {
+    expect(dataTest).toMatch(/JS/);
+});
+
+test('The title contains React', () => {
+    expect(dataTest).toContain('React');
+});
+
+// Arrays
+const data2 = ['React Native', 'MeteorJS'];
+
+test('The list of courses mentions React Native and MeteorJS', () => {
+    expect(['React Native', 'MeteorJS', 'React'])
+        .toEqual(expect.arrayContaining(data2));
+});
+
+// Objects
+test('The first course to have a property of Title', () => {
+    expect(data[0]).toHaveProperty('title');
+});
+
+test('The number of view to be 31,266', () => {
+    expect(data[0]).toHaveProperty('views', '31,266');
+});
